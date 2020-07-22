b5815017a6f0ddd482b8247b9b01da9cf9209c58
AQadir64 <a.qadir67@outlook.com>
2020-07-22 18:27:54 +0500

    setup : github apollo server

---
+++ src/ApolloProvider.js
@@ -0,0 +1,26 @@
+import React, { Suspense } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { openSourceSection } from "./techfolio";
+import Loader from "./Components/common/Loader.js";
+const App = React.lazy(() => import("./App"));
+const key = openSourceSection.githubToken.replace(/^"(.*)"$/, "$1");
+console.log();
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${atob(key)}`,
+      },
+    });
+  },
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <Suspense fallback={<Loader />}>
+      <App />
+    </Suspense>
+  </ApolloProvider>
+);
