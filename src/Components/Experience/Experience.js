1c18205b167656b47e060fc43d9d44e5e9e9bcfa
AQadir64 <a.qadir67@outlook.com>
2020-07-22 18:32:38 +0500

    added : techfolio experience section

---
+++ src/Components/Experience/Experience.js
@@ -0,0 +1,66 @@
+import React, { Fragment } from "react";
+import ExperienceCard from "./ExperienceCard";
+import Fade from "react-reveal/Fade";
+import "./experience.css";
+import { experienceSection } from "../../techfolio";
+import Heading from "../common/Heading";
+import { isMobile } from "react-device-detect";
+
+function Experience() {
+  const experiences = experienceSection.experiences.map((experience, i) => {
+    return <ExperienceCard experience={experience} key={i} />;
+  });
+  return (
+    <Fragment>
+      {isMobile && <Heading heading={"Experience"} />}
+      <section
+        className="section section6"
+        id="experiencesSection"
+        name="experiences"
+      >
+        <div>
+          <Fade bottom>
+            <div className="container experience-row-animation">
+              <div>
+                <div
+                  data-vc-full-width="true"
+                  data-vc-full-width-init="true"
+                  className="vc_row wpb_row vc_row-fluid vc_custom_1542873226451 vc_row-has-fill experience-row-animation"
+                  style={{
+                    left: "-11.2px",
+                    boxSizing: "border-box",
+                    paddingLeft: "11.2px",
+                    paddingRight: "10.8px",
+                    position: "relative",
+                  }}
+                >
+                  <div className="experience_column vc_column_container vc_col-sm-12">
+                    <div className="vc_column-inner ">
+                      <div className="wpb_wrapper">
+                        <div className="experience_spacing">
+                          <div
+                            className="spacing_size spacing_size-initial"
+                            style={{ height: "30px" }}
+                          ></div>
+                        </div>
+                        <div className="experience_time_line_vertical appear_anim">
+                          {experiences}
+                        </div>
+                        <div
+                          id="seofy_spacer_5cf90ca8190c4"
+                          className="experience_spacing responsive_active"
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Fade>
+        </div>
+      </section>
+    </Fragment>
+  );
+}
+
+export default Experience;
---
+++ src/Components/Experience/ExperienceCard.js
@@ -0,0 +1,58 @@
+import React, { useContext } from "react";
+import VisibilitySensor from "react-visibility-sensor";
+import { ScrollContext } from "../../Context/scroll";
+function ExperienceCard({ experience }) {
+  const { scrollChange } = useContext(ScrollContext);
+  return (
+    <div id="time_line_5cf90ca818f641" className="time_line-item  item_show">
+      <div className="time_line-date_wrap">
+        <div className="seofy_hexagon">
+          <svg
+            style={{ fill: " #7f5af0" }}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 177.4 197.4"
+          >
+            <path d="M0,58.4v79.9c0,6.5,3.5,12.6,9.2,15.8l70.5,40.2c5.6,3.2,12.4,3.2,18,0l70.5-40.2c5.7-3.2,9.2-9.3,9.2-15.8V58.4 c0-6.5-3.5-12.6-9.2-15.8L97.7,2.4c-5.6-3.2-12.4-3.2-18,0L9.2,42.5C3.5,45.8,0,51.8,0,58.4z"></path>
+          </svg>
+        </div>
+        <div className="seofy_hexagon">
+          <svg
+            style={{
+              fill: " #7f5af0",
+              filter: "drop-shadow(4px 5px 4px #7f5af022)",
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 177.4 197.4"
+          >
+            <path d="M0,58.4v79.9c0,6.5,3.5,12.6,9.2,15.8l70.5,40.2c5.6,3.2,12.4,3.2,18,0l70.5-40.2c5.7-3.2,9.2-9.3,9.2-15.8V58.4 c0-6.5-3.5-12.6-9.2-15.8L97.7,2.4c-5.6-3.2-12.4-3.2-18,0L9.2,42.5C3.5,45.8,0,51.8,0,58.4z"></path>
+          </svg>
+        </div>
+
+        <VisibilitySensor
+          onChange={(isVisible) => {
+            if (isVisible) {
+              scrollChange("experiences");
+            }
+          }}
+        >
+          <h4 className="time_line-date">{experience.experienceDuration}</h4>
+        </VisibilitySensor>
+      </div>
+      <div className="time_line-content">
+        <h5 className="time_line-title">{experience.experienceTitle}</h5>
+        <div className="time_line-descr">{experience.experienceDesc}</div>
+        <div className="languages">
+          <div className="languages-profile">
+            <div className="languages-name">
+              {experience.experienceTags.map((tag, i) => {
+                return <span key={i}>{tag}</span>;
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExperienceCard;
