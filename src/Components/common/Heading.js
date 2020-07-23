2b5ac1ab752fe36cbd6c1123e381fad9942e00fd
AQadir64 <a.qadir67@outlook.com>
2020-07-23 15:31:58 +0500

    ui fix : mobile heading margin

--- src/Components/common/Heading.js
+++ src/Components/common/Heading.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Col, Row } from "react-flexbox-grid";
 import Fade from "react-reveal/Fade";
-function Heading({ heading, style }) {
+function Heading({ heading}) {
   return (
     <Row center="xs">
       <Col xs={8}>
         <div className="banner-content">
-          <h1>
+          <h1 style={{textShadow: "2px 2px #7f5af0"}}>
             {" "}
             <Fade bottom big cascade>
               {heading}
