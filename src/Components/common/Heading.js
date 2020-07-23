7f62a658d136d3441384b586a2e6f3d57019c099
AQadir64 <a.qadir67@outlook.com>
2020-07-23 10:56:57 +0500

    update : mobile heading components content

--- src/Components/common/Heading.js
+++ src/Components/common/Heading.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Col, Row } from "react-flexbox-grid";
 import Fade from "react-reveal/Fade";
-function Heading({ heading ,style}) {
+function Heading({ heading, style }) {
   return (
     <Row center="xs">
       <Col xs={8}>
-        <div className="brand">
-          <h1 className="logo logo--stroke logo--shadow">
+        <div className="banner-content">
+          <h1>
+            {" "}
             <Fade bottom big cascade>
               {heading}
             </Fade>
